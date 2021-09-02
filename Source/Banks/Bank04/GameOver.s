2f093757649076a3a5e210f60ed01d466ad4dc14
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-09-02 13:21:56 -0400

    fill in temporary files for sounds

---
+++ Source/Banks/Bank04/GameOver.s
@@ -0,0 +1,40 @@
+;;; Music compiled from Source/Banks/Bank07/GameOver.midi;
+;;; do not bother editing (generated file will be overwritten)
+	.if TV == NTSC
+
+
+;;;
+SongGameOver:
+	;; (Piano)
+	;; rest
+	.sound $4, $1, $05,  14, 0	; E
+	;; rest
+	.sound $4, $1, $04,  14, 0	; G
+	;; rest
+	.sound $4, $1, $05,  14, 0	; E
+	;; rest
+	.sound $4, $1, $05,  28, 0	; E
+	.sound $0, $0, $00,   2, 0	; rest
+	.sound $4, $1, $07,  57, 0	; C
+	.sound 0, 0, 0, 0, 1	; rest
+
+;;; end of SongGameOver
+	.else ; PAL or SECAM
+;;;
+SongGameOver:
+	;; (Piano)
+	;; rest
+	.sound $4, $1, $05,  14, 0	; E
+	;; rest
+	.sound $4, $1, $04,  14, 0	; G
+	;; rest
+	.sound $4, $1, $05,  14, 0	; E
+	;; rest
+	.sound $4, $1, $05,  28, 0	; E
+	.sound $0, $0, $00,   2, 0	; rest
+	.sound $4, $1, $07,  57, 0	; C
+	.sound 0, 0, 0, 0, 1	; rest
+
+;;; end of SongGameOver
+
+	.fi
