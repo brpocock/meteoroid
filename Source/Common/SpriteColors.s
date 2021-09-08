1640da18a03ed1460ac9dadf813ce1e5659e160c
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-09-08 18:09:17 -0400

    art updates

---
+++ Source/Common/SpriteColors.s
@@ -0,0 +1,13 @@
+;;; Meteoroid Source/Common/SpriteColors.s
+;;; Copyright © 2021 Bruce-Robert Pocock
+
+SpriteColors:       .block
+
+          .colu COLYELLOW, $e
+          .colu COLORANGE, $e
+          .colu COLBLUE, $e
+          .colu COLGREEN, $e
+          .colu COLPURPLE, $e
+          .colu COLRED, $e
+
+          .bend
